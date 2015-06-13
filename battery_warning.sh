7b079b79de1435caeb282ce18bda7a28000895a1
javl <mail@jaspervanloenen.com>
2015-06-13 11:43:00 +0200

    Changed battery warning to show at 5%

--- battery_warning.sh
+++ battery_warning.sh
@@ -7,7 +7,7 @@ clear
 # Start blinking the power button light. Useful for spotting an
 # empty battery from a distance
 echo "0 blink" > /proc/acpi/ibm/led
-zenity --warning --width 200 --text "Battery is getting low (8%)" 2>/dev/null
+zenity --warning --width 200 --text "Battery is getting low (5%)" 2>/dev/null
 # After the zenity warning has been closed, make the LED solid again
 echo "0 on" > /proc/acpi/ibm/led
 exit
